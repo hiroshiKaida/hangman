548b99e73b2a139b324cdbe363cce97db5eda169
hiroshiKaida <h_kaida@me.com>
2022-11-04 15:38:09 +0900

    Adjust pycodestyle.

--- print_prime.py
+++ print_prime.py
@@ -1,7 +1,9 @@
+"""Create a break example."""
+
 for n in range(2, 30):
-  for x in range(2, n):
-    if n % x == 0:
-      print(n, " equals", x, "*", n/x)
-      break
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, " equals", x, "*", n/x)
+            break
     else:
-      print(n, " is a prime number.")
+        print(n, " is a prime number.")
